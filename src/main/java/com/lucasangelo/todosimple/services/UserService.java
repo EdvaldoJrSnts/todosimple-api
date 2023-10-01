98a0ff6af6f6b337bdfd8330ce27e8fbc76d079c
EdvaldoJrSnts <edvaldojr.santos91@gmail.com>
2023-10-01 16:36:38 -0300

    Create UserService

--- src/main/java/com/lucasangelo/todosimple/services/UserService.java
+++ src/main/java/com/lucasangelo/todosimple/services/UserService.java
@@ -1,13 +1,12 @@
 package com.lucasangelo.todosimple.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.lucasangelo.todosimple.models.User;
 import com.lucasangelo.todosimple.repositories.TaskRepository;
 import com.lucasangelo.todosimple.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
@@ -23,6 +22,29 @@ public class UserService {
         return user.orElseThrow(()-> new RuntimeException(
             "Usuário não encontrado! Id: " + id + ", Tipo: "+User.class.getName()
         ));
+    }
 
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj=this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+
+        }
     }
 }
