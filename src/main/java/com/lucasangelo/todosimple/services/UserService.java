f6c62bcc3ade41a0dce614d9846ebe200563ff15
EdvaldoJrSnts <edvaldojr.santos91@gmail.com>
2023-10-01 15:28:26 -0300

    Create User Service

---
+++ src/main/java/com/lucasangelo/todosimple/services/UserService.java
@@ -0,0 +1,28 @@
+package com.lucasangelo.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasangelo.todosimple.models.User;
+import com.lucasangelo.todosimple.repositories.TaskRepository;
+import com.lucasangelo.todosimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+
+        return user.orElseThrow(()-> new RuntimeException(
+            "Usuário não encontrado! Id: " + id + ", Tipo: "+User.class.getName()
+        ));
+
+    }
+}
